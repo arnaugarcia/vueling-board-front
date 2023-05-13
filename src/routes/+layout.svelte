cd262444531a6772d9a2dc418eb9288f6a8576bc
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2023-05-13 04:03:39 +0200

    Extracted layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+    import MenuComponent from '/src/layout/menu.svelte';
+    import FooterComponent from '/src/layout/footer.svelte';
+    import NavbarComponent from '/src/layout/navbar.svelte';
+</script>
+<div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+
+        <MenuComponent />
+
+        <!-- Layout container -->
+        <div class="layout-page">
+            <!-- Navbar -->
+
+            <NavbarComponent/>
+
+            <!-- Content wrapper -->
+            <div class="content-wrapper">
+                <!-- Content -->
+
+                <div class="container-xxl flex-grow-1 container-p-y">
+                    <slot></slot>
+                </div>
+                <!-- / Content -->
+
+                <FooterComponent/>
+
+                <div class="content-backdrop fade"></div>
+            </div>
+            <!-- Content wrapper -->
+        </div>
+        <!-- / Layout page -->
+    </div>
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+
+    <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+    <div class="drag-target"></div>
+</div>
