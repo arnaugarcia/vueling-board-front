99dbbb0e3a3d64d20d3f0fe3595c8beec472361a
lucastolon <liltolon@gmail.com>
2023-05-13 23:58:38 +0200

    Destination Information

---
+++ src/routes/destinationInformation/+page.svelte
@@ -0,0 +1,64 @@
+<div class="container-xxl flex-grow-1 container-p-y">
+    <div class="row">
+        <div class="col-md">
+            <h4 class="fw-bold py-3 mb-4">Paris</h4>
+
+            <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-bs-target="#carouselExample" data-bs-slide-to="0" class="active"></li>
+                    <li data-bs-target="#carouselExample" data-bs-slide-to="1"></li>
+                    <li data-bs-target="#carouselExample" data-bs-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="../../assets/img/elements/paris1.jpg" alt="First slide" />
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="../../assets/img/elements/paris2.jpg" alt="Second slide" />
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="../../assets/img/elements/paris3.jpg" alt="Third slide" />
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExample" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExample" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-md mt-5">
+
+            <h4 class="fw-bold py-3 mb-4"></h4>
+
+            <div class="card-body">
+                <p>
+                    Paris, the capital of France, is known as the "City of Light" and is one of the world's most visited cities. The city is steeped in history and is home to many fascinating and curious stories that make it a unique destination.
+
+                    Paris dates back to around 250 BC when it was founded by the Parisii, a Celtic tribe. The city has played a significant role in many pivotal moments in history, including the French Revolution, the World Wars, and the 1968 student riots. Today, it remains a center for culture, fashion, and art.
+
+                    One of the most famous landmarks in Paris is the Eiffel Tower, which was built for the 1889 World's Fair. The tower was initially intended to be a temporary structure but proved to be so popular that it was eventually made a permanent fixture. It stands at 324 meters tall and is an iconic symbol of Paris and France.
+
+                    Another historical attraction in Paris is the Palace of Versailles. The palace was the residence of the French monarchy from 1682 until the French Revolution in 1789. It was designed to be a symbol of the power and wealth of the French monarchy and is known for its opulent architecture and ornate gardens.
+
+                    The Louvre Museum is also a must-visit destination in Paris. The museum is home to some of the world's most famous works of art, including the Mona Lisa by Leonardo da Vinci. The Louvre was originally built as a fortress in the 12th century and was converted into a museum in the 18th century.
+
+                    Paris is also famous for its food and drink. French cuisine is renowned for its quality and sophistication, and Paris is home to many Michelin-starred restaurants. One of the most famous French dishes is escargots, or snails, which are often served in Parisian restaurants.
+
+                    In terms of curiosities, Paris is home to some unique attractions. For example, the Catacombs of Paris are an underground network of tunnels that contain the remains of over six million people. The tunnels were created in the 18th century to solve the problem of overflowing cemeteries in the city.
+
+                    Another interesting feature of Paris is the Père Lachaise Cemetery. The cemetery is the final resting place of many famous people, including Jim Morrison, Oscar Wilde, and Edith Piaf. The cemetery is also known for its elaborate tombstones and mausoleums.
+
+                    In conclusion, Paris is a city with a rich history and many interesting curiosities. Whether you are interested in art, history, food, or culture, Paris is sure to have something to offer.
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+
+    <hr class="container-m-nx mt-5" />
+</div>
