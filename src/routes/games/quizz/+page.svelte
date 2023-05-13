8b3fccc98cadbf1b17c4fe80974592907042066d
raimonvelasco <raimonvelasco6@gmail.com>
2023-05-13 20:43:42 +0200

    Backend inteegration

--- src/routes/games/quizz/+page.svelte
+++ src/routes/games/quizz/+page.svelte
@@ -2,9 +2,14 @@
     @import '/css/pages/page-faq.css';
 </style>
 <script>
-
     import AnswerComponent from '/src/components/quizz/answer.svelte'
-
+    let question = 'prueba1'
+    fetch ('http://localhost:3000/questions')
+        .then(respones => respones.json())
+        .then(data => {
+            console.log(data[0].questionText);
+            question = data[0].questionText;
+        });
     let answers = [
         {id : '0', title : 'Respuesta 1'},
         {id : '1', title : 'Respuesta 2'},
@@ -15,7 +20,7 @@
 
 <div class="container-xxl flex-grow-1 container-p-y">
     <div class="faq-header d-flex flex-column justify-content-center align-items-center rounded">
-        <h3 class="text-center">Pregunta :)</h3>
+        <h3 class="text-center">{question}</h3>
     </div>
 </div>
 <div class="row">
