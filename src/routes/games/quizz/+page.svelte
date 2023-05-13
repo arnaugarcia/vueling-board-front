d3ef612f967a8c86c47ec8685e3233feea866b56
raimonvelasco <raimonvelasco6@gmail.com>
2023-05-14 01:10:54 +0200

    Integrated opcions

--- src/routes/games/quizz/+page.svelte
+++ src/routes/games/quizz/+page.svelte
@@ -4,24 +4,37 @@
 <script>
     import AnswerComponent from '/src/components/quizz/answer.svelte'
     import {onMount} from "svelte";
-    let question = 'prueba1'
-
-    onMount(() => {
-        fetch ('http://localhost:3000/questions')
-            .then(responses => responses.json())
-            .then(data => {
-                console.log(data);
-                question = data[0].questionText;
-            });
-    })
-
-
     let answers = [
         {id: '0', title: 'Respuesta 1'},
         {id: '1', title: 'Respuesta 2'},
         {id: '2', title: 'Respuesta 3'},
         {id: '3', title: 'Respuesta 4'}
     ];
+    let question = 'prueba1'
+    let id = 0;
+    let opciones = 'http://localhost:3000/questions/'+id+'/options';
+    onMount(() => {
+        fetch ('http://localhost:3000/questions')
+            .then(responses => responses.json())
+            .then(data => {
+                console.log(data);
+                question = data.questionText;
+                id = data.id;
+                console.log(id);
+            });
+
+    })
+    onMount(()=>{
+        fetch (opciones)
+            .then(responses => responses.json())
+            .then(data => {
+                console.log(data.optionText);
+                answers[0].title = data[0].title;
+            });
+
+    })
+
+
 
 </script>
 
@@ -33,13 +46,11 @@
 <div class="row">
     <div class="col-lg- mx-auto mb-4">
         <div class="row">
-
             {#each answers as answer, i}
 
                 <AnswerComponent option={answer} index={i}/>
 
             {/each}
-
         </div>
     </div>
 </div>
