11c5852e3b95b3f4340b59d0928b601df96cb565
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2023-05-14 04:17:23 +0200

    Added store websocket logic

---
+++ src/components/chat/store.js
@@ -0,0 +1,26 @@
+import { writable } from 'svelte/store';
+
+const messageStore = writable('');
+
+const socket = new WebSocket('ws://127.0.0.1:4500');
+
+socket.addEventListener('open', function (event) {
+    console.log("It's open");
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    messageStore.set(event.data);
+});
+
+const sendMessage = (message) => {
+    if (socket.readyState <= 1) {
+        socket.send(message);
+    }
+}
+
+
+export default {
+    subscribe: messageStore.subscribe,
+    sendMessage
+}
